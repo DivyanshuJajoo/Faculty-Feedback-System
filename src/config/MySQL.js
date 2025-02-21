60f47630b32e96b297178e147bab18690f7132d0
DivyanshuJajoo <divyanshujajoo1@gmail.com>
2025-02-21 08:36:38 +0530

    Feedback system base model

---
+++ src/config/MySQL.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise';
+
+// Create a connection pool
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: 'root',    // Your MySQL username
+  password: 'Jajoo@2001', // Your MySQL password
+  database: process.env.DB_NAME, // Your database name
+  port: process.env.DB_PORT, // Default MySQL port
+});
+
+// Function to check DB connection
+export const connectToDB = async () => {
+  try {
+    const connection = await db.getConnection(); // Ensure the connection works
+    console.log('Connected to MySQL database');
+    connection.release(); // Release connection back to the pool
+  } catch (err) {
+    console.error('Error connecting to MySQL database:', err.stack);
+  }
+};
+
+// Function to get the DB pool for queries
+export const getDB = () => {
+  return db; // Return the pool, which can be used for queries
+};
